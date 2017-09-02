646d01a1b42b1177b2c40d96ca13bbce76b4ae18
aykutuludag1@gmail.com <aykut138911>
2017-09-02 17:56:13 +0300

    alpha

--- app/src/main/java/com/isend/FragmentAccounts.java
+++ app/src/main/java/com/isend/FragmentAccounts.java
@@ -1,6 +1,14 @@
 package com.isend;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +25,7 @@ import com.google.android.gms.common.SignInButton;
 public class FragmentAccounts extends Fragment {
 
     CallbackManager callbackManager;
+    private static final int REQUEST_CALENDAR_READ = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,7 +39,7 @@ public class FragmentAccounts extends Fragment {
             public void onClick(View v) {
                 switch (v.getId()) {
                     case R.id.account_google:
-                        Toast.makeText(getActivity(), getString(R.string.base_account), Toast.LENGTH_SHORT).show();
+                        googleCalendar();
                         break;
                 }
             }
@@ -61,5 +70,54 @@ public class FragmentAccounts extends Fragment {
         });
 
         return v;
+    }
+
+    void googleCalendar() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CALENDAR}, REQUEST_CALENDAR_READ);
+        }
+        Cursor cur;
+        ContentResolver cr = getActivity().getContentResolver();
+
+        String[] mProjection =
+                {
+                        "_id",
+                        CalendarContract.Events.TITLE,
+                        CalendarContract.Events.EVENT_LOCATION,
+                        CalendarContract.Events.DTSTART,
+                        CalendarContract.Events.DTEND,
+                };
+
+        Uri uri = CalendarContract.Events.CONTENT_URI;
+        cur = cr.query(uri, mProjection, null, null, null);
+
+        if (cur != null) {
+            while (cur.moveToNext()) {
+                String title = cur.getString(cur.getColumnIndex(CalendarContract.Events.TITLE));
+                String start = cur.getString(cur.getColumnIndex(CalendarContract.Events.DTSTART));
+                String end = cur.getString(cur.getColumnIndex(CalendarContract.Events.DTEND));
+                String location = cur.getString(cur.getColumnIndex(CalendarContract.Events.EVENT_LOCATION));
+                System.out.println("Etkinlik adı:" + title + "saat:" + start + "-" + end + "konum:" + location);
+            }
+            cur.close();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CALENDAR_READ: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getActivity(), "sdfsdfdfsdfdfd.", Toast.LENGTH_SHORT).show();
+                    googleCalendar();
+                } else {
+                    Toast.makeText(getActivity(), getString(R.string.error_aborted), Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
